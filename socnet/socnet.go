d60c43aecea21637859156cd4c93dad7a1b5dfae
Guillaume R <guillaume.rondon@gmail.com>
2020-06-19 23:20:40 +0200

    Added a date field (as string)

--- socnet/socnet.go
+++ socnet/socnet.go
@@ -2,30 +2,35 @@ package socnet
 
 // SocialNetwork is a collection of timelines
 type SocialNetwork struct {
-	Timelines		[]Timeline
-} 
+	Timelines []Timeline
+}
 
 // Timeline is a collection of messages for a given user name
 type Timeline struct {
-	Name 		string
-	Messages 	[]string
+	Name     string
+	Messages []Message
+}
+
+type Message struct {
+	Text string
+	Date string
 }
 
 // ReadTimeline returns the list of messages for a given user name
-func (socnet SocialNetwork) ReadTimeline(name string) []string {
+func (socnet SocialNetwork) ReadTimeline(name string) []Message {
 	for i := 0; i < len(socnet.Timelines); i++ {
 		if socnet.Timelines[i].Name == name {
 			return reverseMessageLog(socnet.Timelines[i].Messages)
 		}
 	}
-	return []string{}
+	return []Message{}
 }
 
-func reverseMessageLog(messages []string) []string  {
-	var reversedList []string
+func reverseMessageLog(messages []Message) []Message {
+	var reversedList []Message
 
 	for i := len(messages); i > 0; i-- {
-		reversedList = append(reversedList, messages[i - 1])
-	}	
+		reversedList = append(reversedList, messages[i-1])
+	}
 	return reversedList
 }
--- socnet/socnet_test.go
+++ socnet/socnet_test.go
@@ -4,14 +4,13 @@ import (
 	"testing"
 )
 
-
 func TestReadTimeline(t *testing.T) {
-	t.Run("should return a message published in a personnal timeline", func(t *testing.T)  {
-		message := []string{"I love the weather today"}
+	t.Run("should return a message published in a personnal timeline", func(t *testing.T) {
+		message := []Message{Message{Text: "I love the weather today", Date: "19-06-2020"}}
 		timeline := &Timeline{"Alice", message}
 		socnet := SocialNetwork{[]Timeline{*timeline}}
 
-		expected := []string{"I love the weather today"}
+		expected := []Message{Message{Text: "I love the weather today", Date: "19-06-2020"}}
 		got := socnet.ReadTimeline("Alice")
 		if got[0] != expected[0] {
 			t.Errorf("got %q expected %q", got, expected)
@@ -19,11 +18,11 @@ func TestReadTimeline(t *testing.T) {
 	})
 
 	t.Run("should return a list of messages published in a personnal timeline", func(t *testing.T) {
-		message := []string{"Damn! We lost!", "Good game though."}
+		message := []Message{Message{Text: "Damn! We lost!", Date: "123"},Message{Text: "Good game though.", Date: "toto"}}
 		timeline := &Timeline{"Bob", message}
 		socnet := SocialNetwork{[]Timeline{*timeline}}
 
-		expected := []string{"Good game though.", "Damn! We lost!"}
+		expected := []Message{Message{Text: "Good game though.", Date: "toto"},Message{Text: "Damn! We lost!", Date: "123"}}
 		got := socnet.ReadTimeline("Bob")
 
 		if got[0] != expected[0] {
