4633fa742fa6b7f4aa6c33d09c095df0a30015e3
Guillaume R <guillaume.rondon@gmail.com>
2020-06-19 23:21:31 +0200

    Linting

--- socnet/socnet_test.go
+++ socnet/socnet_test.go
@@ -6,11 +6,11 @@ import (
 
 func TestReadTimeline(t *testing.T) {
 	t.Run("should return a message published in a personnal timeline", func(t *testing.T) {
-		message := []Message{Message{Text: "I love the weather today", Date: "19-06-2020"}}
+		message := []Message{{Text: "I love the weather today", Date: "19-06-2020"}}
 		timeline := &Timeline{"Alice", message}
 		socnet := SocialNetwork{[]Timeline{*timeline}}
 
-		expected := []Message{Message{Text: "I love the weather today", Date: "19-06-2020"}}
+		expected := []Message{{Text: "I love the weather today", Date: "19-06-2020"}}
 		got := socnet.ReadTimeline("Alice")
 		if got[0] != expected[0] {
 			t.Errorf("got %q expected %q", got, expected)
@@ -18,11 +18,11 @@ func TestReadTimeline(t *testing.T) {
 	})
 
 	t.Run("should return a list of messages published in a personnal timeline", func(t *testing.T) {
-		message := []Message{Message{Text: "Damn! We lost!", Date: "123"},Message{Text: "Good game though.", Date: "toto"}}
+		message := []Message{{Text: "Damn! We lost!", Date: "123"}, {Text: "Good game though.", Date: "toto"}}
 		timeline := &Timeline{"Bob", message}
 		socnet := SocialNetwork{[]Timeline{*timeline}}
 
-		expected := []Message{Message{Text: "Good game though.", Date: "toto"},Message{Text: "Damn! We lost!", Date: "123"}}
+		expected := []Message{{Text: "Good game though.", Date: "toto"}, {Text: "Damn! We lost!", Date: "123"}}
 		got := socnet.ReadTimeline("Bob")
 
 		if got[0] != expected[0] {
